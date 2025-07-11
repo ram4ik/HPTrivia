5619cc60007cb067747caa150337f80a3b14c287
ribragimov-mwb <ribragimov@malwarebytes.com>
2025-07-11 13:16:27 +0300

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Ramill Ibragimov on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
